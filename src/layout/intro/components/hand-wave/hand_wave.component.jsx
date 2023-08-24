5e631f96f121787a579262bda347b70c0e5b5ac2
WellesleyMussolini <iei.muss@gmail.com>
2023-08-23 22:25:43 -0300

    first commit

---
+++ src/layout/intro/components/hand-wave/hand_wave.component.jsx
@@ -0,0 +1,9 @@
+import styles from "./hand_wave.module.css";
+
+const HandWave = () => (
+    <span className={styles.wave} role="img">
+        👋🏻
+    </span>
+);
+
+export default HandWave;
--- src/layout/intro/introduction.jsx
+++ src/layout/intro/introduction.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import styles from "./introduction.module.css";
 import Typed from "react-typed";
 import StarsAnimation from "./components/stars-animation/stars.component";
+import HandWave from "./components/hand-wave/hand_wave.component";
 
 const Introduction = () => {
     return (
         <div className={styles.container}>
             <StarsAnimation />
             <div className={styles.wrapper}>
+                {/* DONE */}
+                <div>
+                    <h1 className={styles.title}>
+                        Olá, eu sou</h1>
+                    <h1 className={styles.title}><strong style={{ fontWeight: "bolder" }}>Wellesley Mussolini</strong>
+                        <HandWave />
+                    </h1>
+                </div>
+
                 {/* DONE */}
                 <p className={styles.skills}>
                     <strong>
