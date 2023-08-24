7aa08807dd16a070b4c6f63d741f62ff05e85508
WellesleyMussolini <iei.muss@gmail.com>
2023-08-23 22:12:31 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Home from './pages/home';
+import Introduction from "./layout/intro/introduction.component";
 
 function App() {
   return (
     <div>
-<Home />
+      <Introduction />
     </div>
   );
 }
