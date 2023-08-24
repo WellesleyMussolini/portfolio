5c901c51ea6526f0dd172d60ae68f4e1729dac05
WellesleyMussolini <iei.muss@gmail.com>
2023-08-23 22:21:26 -0300

    first commit

---
+++ src/layout/intro/introduction.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./introduction.module.css";
+import Typed from "react-typed";
+
+const Introduction = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                {/* DONE */}
+                <p className={styles.skills}>
+                    <strong>
+                        <Typed
+                            strings={[
+                                "Front-end Developer",
+                                "Back-end Developer",
+                                "Full Stack Developer",
+                            ]}
+                            typeSpeed={80}
+                            backDelay={2000}
+                            backSpeed={30}
+                            loop
+                        />
+                    </strong>
+                </p>
+            </div>
+        </div >
+    );
+}
+
+export default Introduction;
