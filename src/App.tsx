8aab87fe8f1d10bf573486d20806e58b6ae111bf
WellesleyMussolini <iei.muss@gmail.com>
2023-09-08 13:48:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import Introduction from './layout/intro/introduction';
+import Projects from './layout/projects/projects';
+import Skills from './layout/skills/skills';
+import Contact from './layout/contact/contact';
+
+const App = () => (
+  <>
+    <Introduction />
+    <Projects />
+    {/* <Skills /> */}
+    <Contact />
+  </>
+);
+
+export default App;
